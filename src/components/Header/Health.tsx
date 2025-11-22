020bbacd870632124e1c7ec3a70f9948ab50ba79
Kirills <nightmareahouse@gmail.com>
2025-11-23 00:42:14 +0200

    Add Temp HP and remove in calculator health

--- src/components/Header/Health.tsx
+++ src/components/Header/Health.tsx
@@ -1,12 +1,12 @@
 'use client'
 
-import {useCharacterStore} from '@/store/character'
-import {useState} from 'react'
+import { useCharacterStore } from '@/store/character'
+import { useState } from 'react'
 import Modal from '@/components/Modal'
 import Calculator from '@/components/Calculator'
 
 export default function Health() {
-	const {currentHp, maxHp, updateHp, updateMaxHp, tempHp, updateTempHp} = useCharacterStore()
+	const { currentHp, maxHp, updateHp, updateMaxHp, tempHp, updateTempHp } = useCharacterStore()
 
 	const [isModalOpen, setIsModalOpen] = useState(false)
 	const [newHpValue, setNewHpValue] = useState<string>('0')
@@ -23,7 +23,6 @@ export default function Health() {
 
 	const addTempHp = () => {
 		updateTempHp(Number(newHpValue))
-		updateMaxHp(maxHp + Number(newHpValue))
 		clear()
 	}
 
@@ -47,6 +46,21 @@ export default function Health() {
 
 		const delta = Math.min(parsedAmount, remainingToMin)
 
+		if (tempHp) {
+			const estLiRaznicaVUrone = delta >= tempHp
+
+			if (!estLiRaznicaVUrone) {
+				updateTempHp(-delta)
+			} else {
+				const raznicaVUrone = delta - tempHp
+				updateTempHp(-(delta - raznicaVUrone))
+				updateHp(-raznicaVUrone)
+			}
+
+			clear()
+			return
+		}
+
 		updateHp(-delta)
 		clear()
 	}
@@ -55,10 +69,17 @@ export default function Health() {
 
 	const isRemoveDisabled = currentHp <= -maxHp || parsedAmount <= 0
 
+	const buttonStyle = `bg-[#50459587] hover:bg-[#504595b0] active:bg-[#504595cc]
+	text-white font-semibold px-4 py-2 rounded-xl shadow-md
+	transition-all active:scale-95 w-full
+	disabled:bg-[#50459540] disabled:text-white/40
+	disabled:cursor-not-allowed disabled:shadow-none
+	disabled:hover:bg-[#50459540] disabled:active:scale-100`
+
 	return (
 		<div>
 			<span onClick={openModal} className='cursor-pointer'>
-				Здоровье: {tempHp ? `(${tempHp + currentHp})` : currentHp} / {maxHp}
+				Здоровье: {tempHp > 0 ? `(${tempHp + currentHp})` : currentHp} / {maxHp}
 			</span>
 
 			<Modal isOpen={isModalOpen} onClose={closeModal}>
@@ -71,50 +92,17 @@ export default function Health() {
 				>
 					<div className='flex flex-col items-center w-full mt-2 gap-2'>
 						<div className='w-full'>
-							<button
-								onClick={addTempHp}
-								disabled={parsedAmount <= 0}
-								className='
-                                bg-[#50459587] hover:bg-[#504595b0] active:bg-[#504595cc]
-                                text-white font-semibold px-4 py-2 rounded-xl shadow-md
-                                transition-all active:scale-95 w-full
-                                disabled:bg-[#50459540] disabled:text-white/40
-                                disabled:cursor-not-allowed disabled:shadow-none
-                                disabled:hover:bg-[#50459540] disabled:active:scale-100
-                            '
-							>
+							<button onClick={addTempHp} disabled={parsedAmount <= 0} className={buttonStyle}>
 								Добавить Временное HP
 							</button>
 						</div>
 
 						<div className='flex flex-row gap-6 justify-center w-full'>
-							<button
-								disabled={isAddDisabled}
-								onClick={addHp}
-								className='
-                                bg-[#50459587] hover:bg-[#504595b0] active:bg-[#504595cc]
-                                text-white font-semibold px-4 py-2 rounded-xl shadow-md
-                                transition-all active:scale-95 w-full
-                                disabled:bg-[#50459540] disabled:text-white/40
-                                disabled:cursor-not-allowed disabled:shadow-none
-                                disabled:hover:bg-[#50459540] disabled:active:scale-100
-                            '
-							>
+							<button disabled={isAddDisabled} onClick={addHp} className={buttonStyle}>
 								Добавить HP
 							</button>
 
-							<button
-								disabled={isRemoveDisabled}
-								onClick={removeHp}
-								className='
-                                bg-[#50459587] hover:bg-[#504595b0] active:bg-[#504595cc]
-                                text-white font-semibold px-4 py-2 rounded-xl shadow-md
-                                transition-all active:scale-95 w-full
-                                disabled:bg-[#50459540] disabled:text-white/40
-                                disabled:cursor-not-allowed disabled:shadow-none
-                                disabled:hover:bg-[#50459540] disabled:active:scale-100
-                            '
-							>
+							<button disabled={isRemoveDisabled} onClick={removeHp} className={buttonStyle}>
 								Убрать HP
 							</button>
 						</div>
